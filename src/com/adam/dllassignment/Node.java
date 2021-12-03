f58d798ae4c58aaee8b7523ef46a7308f8da9d75
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-12-02 22:52:52 -0700

    Updated Files

--- src/com/adam/dllassignment/Node.java
+++ src/com/adam/dllassignment/Node.java
@@ -1,5 +1,20 @@
 package com.adam.dllassignment;
 
 public class Node {
-
+	
+	public Integer value;
+	public Node previous;
+	public Node next;
+	
+	
+//	 Constructors
+	public Node(Integer value) {
+		this.value = value;
+		this.previous = null;
+		this.next = null;
+	}
+	
+	
+	
+	
 }
